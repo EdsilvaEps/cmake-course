5d58f28d4ca220d3e3135e740a912f7f838830cb
Edson Silva <Edson.Silva@elektrobit.com>
2022-02-07 12:40:33 +0200

    Using an external package on a local project with cmake

---
+++ module9/app_opencv/main.cpp
@@ -0,0 +1,17 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(){
+
+    Mat image;
+    image = imread("sample.png");
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+
+    return 0;
+
+
+}
+
